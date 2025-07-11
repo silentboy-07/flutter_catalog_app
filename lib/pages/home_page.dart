faa77920ec045bb7c6f7273f5bfb9e9be238505e
Demo-019 <vikasvikisingh@gmail.com>
2025-07-11 20:54:19 +0530

    Day 3 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,7 +8,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Catalog App"), centerTitle: true),
+      appBar: AppBar(
+        title: Text("Catalog App"),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+      ),
       body: Center(child: Text("Welcome to $days Days of Flutter by $name")),
       drawer: Drawer(),
     );
