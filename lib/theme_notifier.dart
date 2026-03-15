5953e18a4c1fd887819cbb6e81450658686b7e5f
Vikas Singh <116294191+silentboy-07@users.noreply.github.com>
2026-03-15 20:17:17 +0530

    Update theme_notifier.dart

--- lib/theme_notifier.dart
+++ lib/theme_notifier.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-// OPTIONAL persistence:
-// import 'package:shared_preferences/shared_preferences.dart';
+
+
 
 class ThemeNotifier extends ChangeNotifier {
-  ThemeMode _themeMode = ThemeMode.system; // start by following system
+  ThemeMode _themeMode = ThemeMode.system; 
   ThemeMode get themeMode => _themeMode;
 
   bool get isDark =>
       _themeMode == ThemeMode.dark ||
-      (_themeMode == ThemeMode.system); // UI helper if needed
+      (_themeMode == ThemeMode.system); 
 
   ThemeNotifier() {
-    // _loadFromPrefs(); // OPTIONAL: uncomment if you implement persistence below
+   
   }
 
   void toggleTheme() {
